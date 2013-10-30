886c88a41e3cd811d8f59068ebe4645829231fb2
Temacotta <ibevandijck@MacBook-Pro-van-Ibe.local>
2013-10-30 22:37:35 +0100

    1.0.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// Google Analytics
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -5,3 +7,18 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-XXXX-Y');
 ga('send', 'pageview');
+
+
+// Disqus comments
+
+var disqus_shortname = 'metacotta';
+
+function loadDisqus() {
+
+	var dsq = document.createElement('script');
+
+	dsq.async = true;
+	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+}
