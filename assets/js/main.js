396c217ce5534d1bde2e1e18633e9731f2a6699a
Temacotta <ibevandijck@MacBook-Pro-van-Ibe.local>
2014-02-10 21:46:42 +0100

    2.0.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,24 @@
 // Google Analytics
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+if(location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
 
-ga('create', 'UA-XXXX-Y');
-ga('send', 'pageview');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-XXXX-Y');
+	ga('send', 'pageview');
+}
 
 
 // Disqus comments
 
 var disqus_shortname = 'metacotta';
 
-function loadDisqus() {
+$('.comments-toggle').on('click', function(event) {
+
+	event.preventDefault();
 
 	var dsq = document.createElement('script');
 
@@ -21,4 +26,4 @@ function loadDisqus() {
 	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 
 	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-}
+});
